2ec0031fb79d97da340223bde0311024c10955c9
unknown <umarhayyat710000@gmail.com>
2023-08-28 17:17:17 +0500

    new commit

---
+++ AssignmentNo2.js
@@ -0,0 +1,14 @@
+"use strict";
+//Write a program to convert the temperature from Celsius to Fahrenheit and vice verse.
+var tempIncelsius = 37;
+var celToF = (tempIncelsius * 9 / 5) + 32;
+console.log(celToF);
+//From Fahrenheit to Celsius.
+var tempInC = 104;
+var fTocelsius = (tempInC * 5 / 9) - 32;
+console.log(fTocelsius);
+// write a program that calculates the percentage.
+let tMarks = 1050;
+let obtainedMark = 694;
+let percentage1 = obtainedMark / tMarks * 100;
+console.log(percentage1);
---
+++ AssignmentNo2.ts
@@ -0,0 +1,15 @@
+//Write a program to convert the temperature from Celsius to Fahrenheit and vice verse.
+// var tempIncelsius:number= 37;
+// var celToF:number= (tempIncelsius*9/5)+32;
+// console.log(celToF);
+//From Fahrenheit to Celsius.
+// var tempInC:number= 104;
+// var fTocelsius:number= (tempInC*5/9)-32;
+// console.log(fTocelsius);
+// write a program that calculates the percentage.
+// let tMarks:number= 1050;
+// let obtainedMark:number= 694;
+// let percentage1:number= obtainedMark/tMarks*100;
+// console.log(percentage1);
+ 
+// write a program that 
